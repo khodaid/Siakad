9c1eee633d7e94b9df8a1eeec96e562e9f97872e
khodaid <khoirulhudaid@gmail.com>
2021-04-20 21:04:15 +0700

    api get

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Akademik\Fakultas;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('fakultas', function () {
+    return Fakultas::all();
 });
